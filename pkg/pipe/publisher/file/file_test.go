c838dc2b2dadbaaaf8694237ceded0532286cd51
Gareth Becker <beckergareth38@gmail.com>
2022-09-14 20:35:34 +0200

    Added test for file publish

---
+++ pkg/pipe/publisher/file/file_test.go
@@ -0,0 +1,25 @@
+package file
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFilePublisher_Publish(t *testing.T) {
+	filePath := "test.txt"
+	defer os.Remove(filePath)
+	publisher := FilePublisher{
+		FilePath: filePath,
+		FileMode: 0644,
+	}
+
+	err := publisher.Publish([]byte("test"))
+	assert.NoError(t, err)
+
+	fileData, err := ioutil.ReadFile(filePath)
+	assert.NoError(t, err)
+	assert.Equal(t, "test", string(fileData))
+}
